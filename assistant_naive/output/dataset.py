b1c68ed63f847a5a3b180a4ef3512002d2d3cc25
Dhiraj Khanal <dhirajkhanal@dhirajs-mbp-2.myfiosgateway.com>
2024-05-29 08:28:19 -0400

    added/edited the dataset

---
+++ assistant_naive/output/dataset.py
@@ -0,0 +1,62 @@
+dataset = [
+  {
+    "question": "What modes does the J&J's ColorSplash XG-W Series Inground Pool and Spa Fixtures offer?",
+    "answer": "The J&J's ColorSplash XG-W Series Inground Pool and Spa Fixtures offer the following modes: \n- RGB-W mode \n- “PureWhite®” mode. \nThese fixtures also include features such as wide-angle LEDs for increased coverage area and synchronization with other ColorSplash series fixtures and lamps."
+  },
+  {
+    "question": "What is the cord length for the model number 23002?",
+    "answer": "The cord length for the model number 23002 is 100 feet"
+  },
+  {
+    "question": "What is the faceplate diameter of the J&J's ColorSplash LED Pool Light?",
+    "answer": "The faceplate diameter of J&J's ColorSplash LED Pool Light is 10.5 inches."
+  },
+  {
+    "question": "What is the part number for the Union Connector Kit for MaxFlo XE pumps?",
+    "answer": "The part number for the Union Connector Kit for MaxFlo XE pumps is SPX3200UNKIT"
+  },
+  {
+    "question": "What is the part number for the Strainer Cover Kit for MaxFlo XE pumps?",
+    "answer": "The part number for the Strainer Cover Kit for MaxFlo XE pumps is SPX3200DLS."
+  },
+  {
+    "question": "What part number corresponds to the Motor Support (VSP) with Base Mount Screws for MaxFlo XE pumps?",
+    "answer": "The part number corresponding to the Motor Support (VSP) with Base Mount Screws for MaxFlo XE pumps is SPX2300G"
+  },
+  {
+    "question": "What is included in the Hardware Pack for MaxFlo XE pumps?",
+    "answer": "The Hardware Pack for MaxFlo XE pumps includes 4 housing bolts, seal-plate spacers, and square nuts"
+  },
+  {
+    "question": "What is the part description for SPXBXTNXE for MaxFlo XE pumps?",
+    "answer": "XE Button Replacement"
+  },
+  {
+      "question": "What is the effective Effective Filtration Area for Model Number S311SXV ProSeriesTM Plus?",
+      "answer": "The effective filtration area for the Model Number S311SXV ProSeriesTM Plus sand filter is 4.95 square feet."
+  },
+  {
+      "question": "What is the compatible Strainer Cover Kit for the MaxFlo® VS with model number SP2300VSP?",
+      "answer": "SPX2300DLS."
+  },
+  {
+      "question": "What is the compatible Strainer Cover O-Ring for the MaxFlo® VS with model number SP2300VSP?",
+      "answer": "SPX2300Z4."
+  },
+  {
+      "question": "What is the compatible Strainer Basket for the MaxFlo® VS with model number SP2300VSP?",
+      "answer": "The compatible Strainer Basket is SPX2330M."
+  },
+  {
+      "question": "What is the compatible Pump Strainer Housing with Drain Plugs for the MaxFlo® VS with model number SP2300VSP?",
+      "answer": "The compatible Pump Strainer Housing with Drain Plugs is SPX2300AA"
+  },
+  {
+      "question": "What is the compatible part number for SP2302VSP model for Diffuser?",
+      "answer": "The compatible Diffuser part number for the MaxFlo® VS SP2302VSP model is SPX2300B"
+  },
+  {
+      "question": "What is the part number for replacement Dome Assembly SV Gray for robotic pool cleaner SharkVac XL?",
+      "answer": "The part number for the replacement Dome Assembly for the SharkVac XL robotic pool cleaner is RCX97471SB-237."
+  }
+]
